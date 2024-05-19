6a39c045f001a68152adaa312ef8f024400d3f0f
nicolas-000 <nicolas.ayala.a@protonmail.ch>
2024-05-18 20:55:33 -0400

    first

---
+++ gestorApp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(req):
+    return render(req, 'gestorApp/index.html')
+
+def dashboard(req):
+    return render(req, 'gestorApp/dashboard.html')
+
+def clientes(req):
+    return render(req, 'gestorApp/clientes.html')
---
+++ tallerMecanico/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from gestorApp import views as vista
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', vista.index,name='index'),
+    path('dashboard/', vista.dashboard,name='dashboard'),
+    path('clientes/', vista.clientes, name='clientes'),
+]
